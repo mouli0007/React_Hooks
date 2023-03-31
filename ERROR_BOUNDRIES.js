642acb164ea9139850ca38438fa023363845301f
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-03-31 10:07:33 +0530

    Create ERROR_BOUNDRIES.js

---
+++ ERROR_BOUNDRIES.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+class ErrorBoundries extends React.Component {
+  state = { hasError: false };
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.log(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+    return this.props.children;
+  }
+}
+
+<ErrorBoundries fallback="Error Occured ">
+  <App />
+</ErrorBoundries>;
