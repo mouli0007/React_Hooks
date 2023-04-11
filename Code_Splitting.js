51aa759dffbf26c5f951a25fa4f1a73de862ffd9
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-04-11 23:25:14 +0530

    Update Code_Splitting.js

--- Code_Splitting.js
+++ Code_Splitting.js
@@ -1,6 +1,6 @@
+############################################
 import React, { lazy, Suspense } from "react";
 import Lazy from "./Lazy";
-
 const Lazy = lazy(() => import("./Lazy"));
 
 const Memo = () => {
@@ -11,6 +11,26 @@ const Memo = () => {
       </Suspense>
     </div>
   );
-};
-
-export default Memo;
+#################################
+ Lazy loading a function !
+ The JS File contains the sum function will only be downloaded when we click the button
+<button
+  onClick={() => {
+    import("../sum.js").then((module) => {
+      alert(module.sum(2, 2));
+    });
+  }}
+>
+  Add Sum
+</button>
+############################
+ Code Splitting a React component when you have a named export
+  Manually  delaying the code for some seconds
+function wait(duration) {
+  return new Promise((res) => setTimeout(res, duration));
+}
+const About = lazy(() =>
+  wait(1000).then((module) => {
+    return { default: module.About };
+  })
+);
